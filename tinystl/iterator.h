9d0cca6b898eb9f7c1798b0aa5eff63c31a10a11
Shumako <yuki@mashiro.link>
2022-08-09 21:54:19 +0800

    Update iterator traits

--- tinystl/iterator.h
+++ tinystl/iterator.h
@@ -75,8 +75,75 @@ namespace tinystl {
     template <class T, class U, bool = has_iterator_cat<iterator_traits<T>>::value>
     struct has_iterator_cat_of : public bool_constant<std::is_convertible<typename iterator_traits<T>::iterator_category, U>::value> {};
 
-    
+    template <class T, class U>
+    struct has_iterator_cat_of<T, U, false> : public false_type {};
+    template <class Iter>
+    struct is_input_iterator : public has_iterator_cat_of<Iter, input_iterator_tag> {};
+    template <class Iter>
+    struct is_output_iterator : public has_iterator_cat_of<Iter, output_iterator_tag> {};
+    template <class Iter>
+    struct is_forward_iterator : public has_iterator_cat_of<Iter, forward_iterator_tag> {};
+    template <class Iter>
+    struct is_bidirectional_iterator : public has_iterator_cat_of<Iter, bidirectional_iterator_tag> {};
+    template <class Iter>
+    struct is_random_access_iterator : public has_iterator_cat_of<Iter, random_access_iterator_tag> {};
 
+    template <class Iterator>
+    struct is_iterator : public bool_constant<is_input_iterator<Iterator>::value || is_output_iterator<Iterator>::value> {};
+
+
+    // template category
+    template <class Iterator>
+    typename iterator_traits<Iterator>::iterator_category iterator_category(const Iterator&) {
+        typedef typename iterator_traits<Iterator>::iterator_category Category;
+        return Category();
+    }
+    // template distance_type
+    template <class Iterator>
+    typename iterator_traits<Iterator>::difference_type* distance_type(const Iterator&) {
+        return static_cast<typename iterator_traits<Iterator>::difference_type*>(0);
+    }
+    // template value_type
+    template <class Iterator>
+    typename iterator_traits<Iterator>::value_type* value_type(const Iterator&) {
+        return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
+    }
+
+
+    // calculate distance between iterators
+    // input_iterator_tag
+    template <class InputIterator>
+    typename iterator_traits<InputIterator>::difference_type
+    distance_dispatch(InputIterator first, InputIterator last, input_iterator_tag) {
+        typename iterator_traits<InputIterator>::difference_type n = 0;
+        while (first != last) { ++first; ++n; } return n;
+    }
+    // random_access_iterator_tag
+    template <class RandomIter>
+    typename iterator_traits<RandomIter>::difference_type
+    distance_dispatch(RandomIter first, RandomIter last, random_access_iterator_tag) {
+        return last - first;
+    }
+
+    template <class InputIterator>
+    typename iterator_traits<InputIterator>::difference_type
+    distance_dispatch(InputIterator first, InputIterator last) {
+        return distance_dispatch(first, last, iterator_category(first));
+    }
+
+    // n steps advance
+    // input_iterator_tag
+    template <class InputIterator, class Distance>
+    void advance_dispatch(InputIterator& i, Distance n, input_iterator_tag) { while (n--) ++i; }
+    // bidirectional_iterator_tag
+    template <class BidirectionalIterator, class Distance>
+    void advance_dispatch(BidirectionalIterator& i, Distance n, bidirectional_iterator_tag) { if (n >= 0) while (n--) ++i; else while (n++) --i; }
+    // random_access_iterator_tag
+    template <class RandomIter, class Distance>
+    void advance_dispatch(RandomIter& i, Distance n, random_access_iterator_tag) { i += n; }
+
+    template <class InputIterator, class Distance>
+    void advance(InputIterator& i, Distance n) { advance_dispatch(i, n, iterator_category(i)); }
 }
 
 #endif
