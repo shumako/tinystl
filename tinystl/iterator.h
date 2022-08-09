2549a32e68fbbcb2331b88fc265c8aac55c7ca0e
Shumako <yuki@mashiro.link>
2022-08-09 22:16:19 +0800

    Update reverse iterator overloads

--- tinystl/iterator.h
+++ tinystl/iterator.h
@@ -144,6 +144,73 @@ namespace tinystl {
 
     template <class InputIterator, class Distance>
     void advance(InputIterator& i, Distance n) { advance_dispatch(i, n, iterator_category(i)); }
+
+
+    // reverse iterator
+    template <class Iterator>
+    class reverse_iterator {
+        private:
+            Iterator current;
+        public:
+            typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+            typedef typename iterator_traits<Iterator>::value_type value_type;
+            typedef typename iterator_traits<Iterator>::difference_type difference_type;
+            typedef typename iterator_traits<Iterator>::pointer pointer;
+            typedef typename iterator_traits<Iterator>::reference reference;
+            typedef typename Iterator iterator_type;
+            typedef typename reverse_iterator<Iterator> self;
+
+            // constructor
+            reverse_iterator() {}
+            explicit reverse_iterator(iterator_type i) : current(i) {}
+            reverse_iterator(const self& rhs) : current(rhs.current) {}
+
+            iterator_type base() const { return current; }
+            reference operator*() const { auto tmp = current; return *--tmp; }
+            pointer operator->() const { return &(operator*()); }
+            self& operator++() { --current; return *this; }
+            self& operator++(int) { self tmp = *this; --current; return tmp; }
+            self& operator--() { ++current; return *this; }
+            self& operator--(int) { self tmp = *this; ++current; return tmp; }
+            self& operator+=(difference_type n) { current -= n; return *this; }
+            self& operator+(difference_type n) const { return self(current - n); }
+            self& operator-=(difference_type n) { current += n; return *this; }
+            self& operator-(difference_type n) const { return self(current + n); }
+            self& operator[](difference_type n) { return *(*this + n); }
+    };
+
+    // overload operator-
+    template <class Iterator>
+    typename reverse_iterator<Iterator>::difference_type operator-(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+        return rhs.base() - lhs.base();
+    }
+
+    // overload logical operator
+    template <class Iterator>
+    bool operator==(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+        return lhs.base() == rhs.base();
+    }
+    template <class Iterator>
+    bool operator<(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+        return rhs.base() < lhs.base();
+    }
+    template <class Iterator>
+    bool operator!=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+        return !(lhs == rhs);
+    }
+    template <class Iterator>
+    bool operator>(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+        return rhs < lhs;
+    }
+    template <class Iterator>
+    bool operator<=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+        return !(rhs < lhs);
+    }
+    template <class Iterator>
+    bool operator>=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+        return !(lhs < rhs);
+    }
+
 }
 
 #endif
